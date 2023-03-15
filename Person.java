699d7288906b2a2e7e6407380430a3eac055006e
BiLen76 <126571888+BiLen76@users.noreply.github.com>
2023-03-15 22:04:30 +0100

    to add my homework No.1 for Java Basic

---
+++ Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    public static void main(String[] args) {
+        String name = "alena";
+        String surname = "bilko";
+        int year0fBirth = 1976;
+
+        System.out.println(name);
+        System.out.println(surname);
+        System.out.println(year0fBirth);
+    }
+
+}
