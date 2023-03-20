404f0270f543f7a94f230f4d31a5488adf63d54a
BiLen76 <126571888+BiLen76@users.noreply.github.com>
2023-03-20 18:04:22 +0100

    to add my homework No.4 for Java Basic

---
+++ StringTest.java
@@ -0,0 +1,26 @@
+package Homeworks;
+
+public class StringTest {
+    public static void main(String[] args) {
+
+        String a = "Hello, Java! It's a Java Basic course";
+
+        System.out.println(a.length());
+
+        char q = '!';
+        System.out.println((int)q);
+
+        char w = 33;
+        System.out.println(w);
+
+        System.out.println(a.substring(11,13));
+
+        System.out.println(a.substring(13,25));
+
+        System.out.println(a.substring(20,37));
+
+
+
+
+    }
+}
